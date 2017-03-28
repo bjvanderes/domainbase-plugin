c79c3da9ddf759a3fbd65cd7adf020f48d1d8f11
bjvanderes <b.j.vanderes@gmail.com>
2017-03-28 18:01:24 -0400

    Initial commit with empty working plugin tool windows

---
+++ src/domainbase/plugin/DomainbaseQueryNavToolWindowFactory.java
@@ -0,0 +1,17 @@
+package domainbase.plugin;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by Brendan on 3/26/2017.
+ */
+public class DomainbaseQueryNavToolWindowFactory implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+    }
+}
